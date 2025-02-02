654969c235850f0df58ff76f5930b9229f72a2fd
chetan71 <91386497+chetan71@users.noreply.github.com>
2025-02-02 23:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-//your JS code here. If required.
+
         const inputs = document.querySelectorAll('.code');
 
         inputs.forEach((input, index) => {
             input.addEventListener('input', (e) => {
                 const value = e.target.value;
-                if (/^\d$/.test(value)) {
+                if (/^\d$/.test(value)) { // Check if the input is a single digit
                     if (index < inputs.length - 1) {
-                        inputs[index + 1].focus();
+                        inputs[index + 1].focus(); // Move focus to the next input
                     }
                 } else {
-                    e.target.value = '';
+                    e.target.value = ''; // Clear the input if it's not a digit
                 }
             });
 
             input.addEventListener('keydown', (e) => {
                 if (e.key === 'Backspace') {
-                    e.preventDefault();
-                    let lastFilledIndex = -1;
-
-                    // Find the last filled input
-                    for (let i = inputs.length - 1; i >= 0; i--) {
-                        if (inputs[i].value !== '') {
-                            lastFilledIndex = i;
-                            break;
+                    if (e.target.value === '') {
+                        // If the current input is empty, move focus to the previous input
+                        if (index > 0) {
+                            inputs[index - 1].focus();
                         }
-                    }
-
-                    if (lastFilledIndex !== -1) {
-                        inputs[lastFilledIndex].value = '';
-                        if (lastFilledIndex > 0) {
-                            inputs[lastFilledIndex - 1].focus();
-                        }
+                    } else {
+                        // If the current input has a value, clear it
+                        e.target.value = '';
                     }
                 }
             });
